5f123aedf5bf16b0d3f5a637466ea2f4f045096b
ryanWang2018 <ryanwei.wang@mail.utoronto.ca>
2019-03-31 15:50:03 -0400

    fewfe

--- client/src/components/gameRooms.jsx
+++ client/src/components/gameRooms.jsx
@@ -28,7 +28,7 @@ class GameRooms extends Component {
           (this.state.curr_page - 1) * 6,
           (this.state.curr_page - 1) * 6 + 6
         );
-        this.setState({ new_rooms });
+        this.setState({ rooms: new_rooms });
         this.handlerGetRoomsLongPolling();
       })
       .catch(err => {
